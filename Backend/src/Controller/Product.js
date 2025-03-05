489ddea80ae26a2ebbf9bcb659d17859513a149d
nayan457-bit <nayan.krishna.s68@kalvium.community>
2025-03-05 16:23:07 +0530

    done

--- Backend/src/Controller/Product.js
+++ Backend/src/Controller/Product.js
@@ -6,6 +6,37 @@ const productrouter=Router();
 const path = require('path');
 const { userInfo } = require('os');
 
+productrouter.put('/edit-cart',async(req,res)=>{
+    const {email,productid,quantity}=req.body
+    try{
+    
+    if(!email||!productid||quantity==undefined){
+     return res.status(404).json({message:"put all details"})
+    }
+    const finduser=await userModel.findOne({email:email})
+    if(!finduser){
+     return res.status(500).json({message:"user is not found"})
+    }
+ 
+    const findproduct=await Productmodel.findOne({_id:productid})
+    if(!findproduct||findproduct.stock<=0){
+     return res.status(404).json({message:"product not avzailable"})
+    }
+   
+    const findcartproduct=finduser.cart.find(item=>item.productid===productid)
+ 
+    if(!findcartproduct){
+     return res.status(404).json({message:"can not find"}) 
+    }
+    findcartproduct.quantity=quantity
+    await finduser.save()
+    return res.status(200).json({message:"edited successfully"})
+ }
+ catch(err){
+     console.log(err)
+ }
+ })
+
 productrouter.get("/get-product", async (req, res) => {
     try {
         const productfind = await Productmodel.find();
